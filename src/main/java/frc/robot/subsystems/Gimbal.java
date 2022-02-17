e9d2d6f7b2a738db06a6aa0d6d30db106986706d
Matt-Cast7 <56056195+Matt-Cast7@users.noreply.github.com>
2022-02-17 14:47:31 -0500

    yo

--- src/main/java/frc/robot/subsystems/Gimbal.java
+++ src/main/java/frc/robot/subsystems/Gimbal.java
@@ -1,1 +1,24 @@
 package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Gimbal extends SubsystemBase{
+    
+    
+    private Servo servox = new Servo(0);
+    private Servo servoy = new Servo(0);
+    
+    
+    public Gimbal(){
+        //add servo.set something//
+    }
+
+    public void setServoX(double value){
+        servox.set(value);
+    }
+    public void setServoY(double value){
+        servoy.set(value);
+    }
+
+}
